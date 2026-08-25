87e707f745ff937f9f99e25a18b16fb0f783a3f6
heygideon <47083178+heygideon@users.noreply.github.com>
2026-08-25 03:12:41 +0100

    fix: user/application table design

--- src/routes/console/manage/apps/index.tsx
+++ src/routes/console/manage/apps/index.tsx
@@ -24,42 +24,52 @@ function RouteComponent() {
 
   return (
     <>
-      <div className="p-8 pb-0">
+      <div className="p-8">
         <div className="flex justify-between gap-4">
-          <h1 className="font-heading text-3xl font-bold">Applications</h1>
+          <h1 className="font-heading min-h-10 text-3xl font-bold">
+            Applications
+          </h1>
           <Dialog.Trigger
             handle={newAppDialog}
-            render={<Button color="primary" />}
+            render={<Button color="primary" size="md" />}
           >
             <PlusIcon strokeWidth={2.5} className="size-4" />
             New
           </Dialog.Trigger>
         </div>
-      </div>
 
-      <div className="flex gap-4 border-b border-neutral-300 px-8 pt-6 pb-2 text-sm">
-        <p className="w-2/5 flex-auto truncate font-semibold">Name</p>
-        <p className="w-2/5 flex-auto truncate font-semibold">Homepage URL</p>
-        <p className="w-1/5 flex-auto truncate font-semibold">Created at</p>
-      </div>
-      {data.map((app) => (
-        <Link
-          key={app.id}
-          from={Route.fullPath}
-          params={{ id: app.id }}
-          to="$id"
-          className="flex h-12 items-center gap-4 border-b border-neutral-300 px-8 transition hover:bg-neutral-200"
-        >
-          <div className="flex w-2/5 flex-auto items-center gap-2">
-            <div className="size-4 rounded-xs bg-linear-to-br from-rose-600 to-red-700"></div>
-            <p className="min-w-0 flex-1 truncate">{app.name}</p>
+        <div className="mt-4 divide-y divide-neutral-200 overflow-clip rounded-lg border border-neutral-200">
+          <div className="flex gap-4 bg-neutral-50 px-3 py-1.5 text-sm">
+            <p className="min-w-0 flex-1 truncate font-medium">Name</p>
+            <p className="w-48 flex-none truncate font-medium">Created at</p>
           </div>
-          <p className="w-2/5 flex-auto truncate">{app.homepageUrl}</p>
-          <p className="w-1/5 flex-auto truncate">
-            {dayjs(app.createdAt).fromNow()}
-          </p>
-        </Link>
-      ))}
+          {data.map((app) => (
+            <Link
+              key={app.id}
+              from={Route.fullPath}
+              params={{ id: app.id }}
+              to="$id"
+              className="group flex h-14 items-center gap-4 px-3 transition hover:bg-neutral-100"
+            >
+              <div className="flex min-w-0 flex-1 items-center gap-2.5">
+                <div className="size-8 rounded-sm bg-linear-to-br from-rose-600 to-red-700"></div>
+                <div className="min-w-0 flex-1">
+                  <p className="text-sm font-medium text-rose-700 group-hover:text-rose-900">
+                    {app.name}
+                  </p>
+                  <p className="text-xs text-neutral-600">{app.homepageUrl}</p>
+                </div>
+              </div>
+              <p
+                title={dayjs(app.createdAt).toString()}
+                className="w-48 flex-none truncate text-sm underline decoration-dotted underline-offset-2"
+              >
+                {dayjs(app.createdAt).fromNow()}
+              </p>
+            </Link>
+          ))}
+        </div>
+      </div>
 
       <NewAppDialog handle={newAppDialog} />
     </>
--- src/routes/console/manage/users/index.tsx
+++ src/routes/console/manage/users/index.tsx
@@ -1,7 +1,7 @@
-import { orpc } from "#/api/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute, Link } from "@tanstack/react-router";
 import dayjs from "dayjs";
+import { orpc } from "#/api/client";
 
 export const Route = createFileRoute("/console/manage/users/")({
   loader: async ({ context }) => {
@@ -14,36 +14,40 @@ function RouteComponent() {
   const { data } = useSuspenseQuery(orpc.users.all.queryOptions());
 
   return (
-    <>
-      <div className="p-8 pb-0">
-        <h1 className="font-heading text-3xl font-bold">Users</h1>
-      </div>
+    <div className="p-8">
+      <h1 className="font-heading min-h-10 text-3xl font-bold">Users</h1>
 
-      <div className="flex gap-4 border-b border-neutral-300 px-8 pt-6 pb-2 text-sm">
-        <p className="w-2/5 flex-auto truncate font-semibold">Name</p>
-        <p className="w-2/5 flex-auto truncate font-semibold">Email</p>
-        <p className="w-1/5 flex-auto truncate font-semibold">Created at</p>
-      </div>
-      {data.map((user) => (
-        <Link
-          key={user.id}
-          from={Route.fullPath}
-          to="$id"
-          params={{ id: user.id }}
-          className="flex h-12 items-center gap-4 border-b border-neutral-300 px-8 transition hover:bg-neutral-200"
-        >
-          <div className="flex w-2/5 flex-auto items-center gap-2">
-            <div className="size-4 rounded-full bg-linear-to-br from-blue-600 to-indigo-700"></div>
-            <p className="min-w-0 flex-1 truncate">
-              {`${user.firstName} ${user.lastName}`}
+      <div className="mt-4 divide-y divide-neutral-200 overflow-clip rounded-lg border border-neutral-200">
+        <div className="flex gap-4 bg-neutral-50 px-3 py-1.5 text-sm">
+          <p className="min-w-0 flex-1 truncate font-medium">Name</p>
+          <p className="w-48 flex-none truncate font-medium">Created at</p>
+        </div>
+        {data.map((user) => (
+          <Link
+            key={user.id}
+            from={Route.fullPath}
+            params={{ id: user.id }}
+            to="$id"
+            className="group flex h-14 items-center gap-4 px-3 transition hover:bg-neutral-100"
+          >
+            <div className="flex min-w-0 flex-1 items-center gap-2.5">
+              <div className="size-8 rounded-full bg-linear-to-br from-blue-600 to-indigo-700"></div>
+              <div className="min-w-0 flex-1">
+                <p className="text-sm font-medium text-rose-700 group-hover:text-rose-900">
+                  {user.firstName} {user.lastName}
+                </p>
+                <p className="text-xs text-neutral-600">{user.email}</p>
+              </div>
+            </div>
+            <p
+              title={dayjs(user.createdAt).toString()}
+              className="w-48 flex-none truncate text-sm underline decoration-dotted underline-offset-2"
+            >
+              {dayjs(user.createdAt).fromNow()}
             </p>
-          </div>
-          <p className="w-2/5 flex-auto truncate">{user.email}</p>
-          <p className="w-1/5 flex-auto truncate">
-            {dayjs(user.createdAt).fromNow()}
-          </p>
-        </Link>
-      ))}
-    </>
+          </Link>
+        ))}
+      </div>
+    </div>
   );
 }
