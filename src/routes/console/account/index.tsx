fb0ca2cea8f7471933f926d96367b75e312ae3a1
heygideon <47083178+heygideon@users.noreply.github.com>
2026-08-27 02:41:36 +0100

    fix: redirect replace on index routes

--- src/routes/console/account/index.tsx
+++ src/routes/console/account/index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/console/account/")({
   beforeLoad: () => {
-    throw redirect({ from: Route.fullPath, to: "./profile" });
+    throw redirect({ from: Route.fullPath, to: "./profile", replace: true });
   },
 });
--- src/routes/console/index.tsx
+++ src/routes/console/index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/console/")({
   beforeLoad: () => {
-    throw redirect({ to: "/console/home" });
+    throw redirect({ to: "/console/home", replace: true });
   },
 });
