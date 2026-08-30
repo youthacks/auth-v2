903173fa764226c773f3a6e0ec8c238d81b54bda
heygideon <47083178+heygideon@users.noreply.github.com>
2026-08-30 17:21:55 +0100

    feat: use s3 for file upload in production

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -1,6 +1,7 @@
 import path from "node:path";
 import { Disk } from "flydrive";
 import { FSDriver } from "flydrive/drivers/fs";
+import { S3Driver } from "flydrive/drivers/s3";
 import { db } from "#/db";
 import { assets } from "#/db/schema/assets";
 
@@ -14,7 +15,21 @@ const fsDriver = new FSDriver({
   },
 });
 
-const disk = new Disk(fsDriver);
+const s3Driver = new S3Driver({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+  bucket: process.env.BUCKET_NAME!,
+  endpoint: process.env.AWS_ENDPOINT_URL_S3,
+  region: process.env.AWS_REGION || "auto",
+  cdnUrl: process.env.AWS_CDN_URL,
+  visibility: "public",
+});
+
+const disk = new Disk(
+  process.env.NODE_ENV === "production" ? s3Driver : fsDriver,
+);
 
 export async function saveAsset(file: File, { ownerId }: { ownerId: string }) {
   const [{ id }] = await db
@@ -27,12 +42,20 @@ export async function saveAsset(file: File, { ownerId }: { ownerId: string }) {
     .returning({ id: assets.id });
 
   const [, key] = id.split(":");
-  await disk.put(key, await file.bytes());
+  try {
+    await disk.put(key, await file.bytes(), {
+      visibility: "public",
+    });
+  } catch (e) {
+    console.error("Error saving asset to disk:", e);
+    throw e;
+  }
 
   return id;
 }
 
 export async function getAssetUrl(id: string) {
   const [, key] = id.split(":");
+
   return disk.getUrl(key);
 }
